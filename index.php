3306a2118190e5bbaaa3fe7fa6e52989400995fb
Eric Morris <eric@zurb.com>
2013-10-08 10:14:19 -0700

    Bump to 0.7.1 because of messup with wrong versions of demo emails.

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       <div class="home-text">
         <h1 class="light">Ink</h1>
         <p class="lead">Quickly create responsive emails that work on any device &amp; client. Even Outlook.</p>
-        <a href="download.php" class="large button">Download Ink 0.7.0</a>
+        <a href="download.php" class="large button">Download Ink 0.7.1</a>
         <div class="inky"></div>
       </div>
     </div>
