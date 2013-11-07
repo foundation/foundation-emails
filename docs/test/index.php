d36b6f3ea95a1ee1b7812dce211acec1943c4539
Eric Morris <eric@zurb.com>
2013-11-06 22:30:27 -0800

    Include marketing site JS in testing harness for docs

--- docs/test/index.php
+++ docs/test/index.php
@@ -23,5 +23,27 @@
 </head>
 <body>
   <?php include 'docs.php' ?>
+  <script src="http://zurb.com/ink/javascripts/vendor/jquery.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.abide.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.alerts.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.clearing.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.cookie.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.dropdown.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.forms.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.interchange.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.joyride.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.magellan.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.orbit.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.placeholder.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.reveal.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.section.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.tooltips.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/foundation.topbar.js"></script>
+  <script src="http://zurb.com/ink/javascripts/foundation/jquery.offcanvas.js"></script>
+  <script src="http://zurb.com/ink/javascripts/app.js"></script>
+  <script type="text/javascript">
+    $(document).foundation();
+  </script>
 </body>
 </html>
