da9eac6f4b463394f71ae4edc0c1f3fe84660bd8
Jeanie Chung <jeanie.chung@gmail.com>
2015-01-30 11:33:54 -0800

    Added a nested components function for nested buttons inside panels. Will refactor later on into a nicer recursive function. For now, we will just have to deal with 'var buttSize'.

--- output/index.js
+++ output/index.js
@@ -14,28 +14,61 @@ module.exports = function (body) {
     var str = file.contents.toString('utf8');
     var $ = cheerio.load(str);
 
-    var insertComponents = function(element, type) {
-      var output     = '';
-      var component  = $(element);
-      var inner      = $(element).html();
-      var compClass  = '';
+    var nestedComponents = function(str) {
+      var output   = '';
+      var children = $(str);
+
+      $(children).each(function(i, el) {
+        var temp = $.html(el);
+
+        if (el.name === 'button') {
+          var compClass = $(el).attr('class') || '';
+          var inner = $(el).html();
+          var buttSize  = 'button';
+
+          if ($(el).attr('size')) {
+            buttSize = $(el).attr('size') + '-button';
+          }
+
+          temp = '<table class="' + buttSize + ' ' + compClass +'"><tbody><tr><td>' + inner + '</td></tr></tbody></table>'
+        }
+
+        output += temp;
+      });
+
+      return output;
+    };
+
+    var componentFactory = function(element, type, content) {
+      var output = '';
+      var component = $(element);
+      var inner = content || $(element).html();
+      var compClass = '';
 
       if ($(component).attr('class')) {
         compClass = $(component).attr('class');
       };
 
+      if ($(element).children('button').length > 0) {
+        inner = nestedComponents(inner);
+      }
       if (type === "panel") {
-        output += '<td class="panel ' + compClass +'">' + inner + '</td>';
+        output = '<td class="panel ' + compClass +'">' + inner + '</td>';
       }
       else if (type === "button") {
-        output += '<td><table class="button ' + compClass +'"><tbody><tr><td>' + inner + '</td></tr></tbody></table></td>'
+        output = '<td><table class="button ' + compClass +'"><tbody><tr><td>' + inner + '</td></tr></tbody></table></td>'
       }
       else {
-        output += '<td>' + inner + '</td>';
+        // unless it's a special element, just grab the inside
+        // another cheerio quirk
+        inner = $.html(element);
+        output = '<td>' + inner + '</td>';
       }
 
       return output;
     };
+
+
 
     var createCol = function(obj) {
       var output   = '';
@@ -43,33 +76,34 @@ module.exports = function (body) {
       // create tables with column class for each nested element
       var colElements = function(elements, colSize) {
         var colHTML = '';
+        var colContent = '';
         $(elements).each(function(i, el) {
           // get included tags of each element in the column
-          var element  = $(el)[0];
-          var elType   = $(el)[0].name;
+          var element  = el;
+          var elType   = el.name;
           var colClass = 'columns ';
-          var content  = '';
 
           // transclude any class that might have been added onto element
           if ($(el).attr('class')) {
             colClass += $(el).attr('class');
           };
-          content = insertComponents(element, elType);
+
+          colContent = componentFactory(element, elType);
 
           // construct column class for each element
-          colHTML += '<table class="' + colClass + ' ' + colSize+'">'
+          colHTML += '<table class="' + colClass + ' ' + colSize + '">'
                   +'<tr>'
-                  + content +'<td class="expander"></td></tr></table>';
+                  + colContent +'<td class="expander"></td></tr></table>';
         });
+
+        // search for still existing elements
 
         return colHTML;
       };
 
       // create tables with wrapper class for each column
-        
-      // weird math thing to do with cheerio including closing tags and stuff
-      // so it throws off the number of objects being counted as elements
-      var colCount = Math.round((obj.length - 1 / 2));
+      
+      var colCount = obj.length;
 
       $(obj).each(function(k,v) {
         var wrapperHTML = '';
@@ -82,15 +116,14 @@ module.exports = function (body) {
         }
 
         // if wrapper is last or the only one, put last class
-
         if (!obj[k].data) {
-          if (k === colCount) {
-            wrapperHTML += '<td class="wrapper ' + colClass + ' last">';
-          } else {
-            wrapperHTML += '<td class="wrapper ' + colClass + '">';
-          }
+          wrapperHTML = '<td class="wrapper ' + colClass + '">';
         }
-
+        // weird math thing to do with cheerio including closing tags and stuff
+        // so it throws off the number of objects being counted as elements
+        if (k === colCount - 3) {
+            wrapperHTML = '<td class="wrapper ' + colClass + 'last">';
+        }
         // check for sizes
         if ($(col).attr('small')) {
           colSize += 'small' + '-' + $(col).attr('small') + ' ';
@@ -104,20 +137,22 @@ module.exports = function (body) {
 
         output += wrapperHTML;
       });
-      
+
       return output;
     };
 
     $('row').each(function(i) {
       var contents = $(this).html();
-
+      var rowClass = '';
       // if row does has nested elements (i.e. not empty)
       if ($(this).children('column')) {
         contents = createCol($(contents));
-
+      }
+      if ($(this).attr('class')) {
+        rowClass = $(this).attr('class');
       }
 
-      $(this).replaceWith('<table class="row"><tbody><tr>'+ contents + '</tr></tbody></table>');
+      $(this).replaceWith('<table class="row ' + rowClass + '"><tbody><tr>'+ contents + '</tr></tbody></table>');
 
     });
 
